79fdb8d2ef988830bc12d1ba72e21eed5e1e1e86
shuvo794 <shuvohossain794@gmail.com>
2021-10-23 20:16:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+     const [users,setUsers]=useState([]);
+     const nameRef=useRef();
+     const emailRef=useRef();
+
+     useEffect(()=>{
+fetch('http://localhost:5000/users')
+.then(res=>res.json())
+.then(data=>setUsers(data))
+
+     },[])
+
+     const handelAdUsers=e=>{
+       const name=nameRef.current.value;
+       const email=emailRef.current.value;
+       const newUser={name:name,email:email};
+fetch('http://localhost:5000/users',{
+  method:'post',
+  headers:{
+    'content-type':'application/json'
+  },
+  body:JSON.stringify(newUser)
+})
+.then()
+
+
+e.preventDefault();
+     }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div className="App">
+<form onSubmit={handelAdUsers}>
+<input type="text" ref={nameRef}  placeholder="Your Name"/>
+<input type="email" name=""ref={emailRef} id=""  placeholder="Your Email"/>
+<input type="submit" value="Submit" />
+</form>
+
+
+
+
+
+
+     <h2>Found :{users?.length}</h2>
+
+
+
+
+     <ul>
+       {
+         users.map(user=><li key={user.id}>{user.id} : {user.name} : {user.email}</li>)
+       }
+     </ul>
     </div>
   );
 }
